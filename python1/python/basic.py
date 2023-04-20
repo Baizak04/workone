d966484ef380ca78111fa5ab0d9ad05c5085f78d
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-20 18:29:07 +0600

    sad

--- python1/python/basic.py
+++ python1/python/basic.py
@@ -6,3 +6,35 @@ name = "alex"
 print(type(name))
 
 
+name = 23
+print(type(name))
+
+
+
+
+
+# import calendar
+   
+# year = 2023
+# month = 11
+   
+
+# print(calendar.month(year, month))
+
+
+import calendar
+year = int(input("Введи: "))
+month = int(input("Год: "))
+
+print(calendar.month(year, month))
+
+
+
+
+
+
+
+# import calendar
+   
+# print ("Calendar 2023 is : ")
+# print (calendar.calendar(2020))
