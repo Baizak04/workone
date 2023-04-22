0b395663146f3c53873d8fd65af318820345edaf
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-22 16:25:06 +0600

    Calculator

--- python1/python/try_except.py
+++ python1/python/try_except.py
@@ -1,16 +1,22 @@
 import sys
 
-# def div(a,b):
-#     try:
-#         print(a/b)
-#     except ZeroDivisionError:
-#         print("деление на ноль")
-#     except TypeError:
-#         print("невозможно выполнить деление, один из параметром не число")
-#     finally:
-#         print("мы делили " + str(a) + " на " + str(b))
 
-# div("a",0)
+print(1 == 2)  #False
+print(1 != 2)  #True
+print(1 > 2)   #False
+print(1 >= 2)  #True
+print(1 < 2)   #False
+print(1 <= 2)  #True
+
+
+operation = input('''
+1 - сложить
+2 - вычесть
+3 - умножить
+4 - разделить
+5 - возвести в степень
+''')
+
 
 
 x1 =input('Введите x1: ')
@@ -30,5 +36,40 @@ except ValueError:
     print('Ошибка: введите число: ')
     sys.exit(2)
 
-print(x1, '**', '=', x1 ** x2)
+
+res = None
+if (operation == '1'):
+    res = x1 + x2
+elif operation == '2':
+    res = x1 - x2
+elif operation == '3':
+    res = x1 * x2
+elif operation == '4':
+    res = x1 / x2
+elif operation == '5':
+    res = x1 ** x2
+else:
+    print('Ошибка: неправильная операция')
+    sys.exit(1)
     
+if (1 == 2 or 2 == 3) and (2 == 2):
+    pass
+ 
+if res is not None:
+    print('res =', res)
+
+    
+    
+    
+    
+def div(a,b):
+    try:
+        print(a/b)
+    except ZeroDivisionError:
+        print("деление на ноль")
+    except TypeError:
+        print("невозможно выполнить деление, один из параметром не число")
+    finally:
+        print("мы делили " + str(a) + " на " + str(b))
+
+div("a",0)
