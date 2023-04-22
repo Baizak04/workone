6e2235ec00d0ee4688b22cb4e35b7c8ad80885de
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-22 17:35:13 +0600

    list.py

--- python1/python/list.py
+++ python1/python/list.py
@@ -1,4 +1,4 @@
-# список [] - измениямый
+# список [list] - измениямый
 
 # методы
 # append, clear, copy, count, extend, index, insert, pop, remove,reverse, sort, sorted 
@@ -9,3 +9,22 @@ children = ['arbuzov', 'ivanov', 'petrov', 'Azat']
 children_2 = sorted(children)
 print(children_2)
 
+x = [1, 2, 3, 4]
+print(x, type(x), len(x))
+x.append(7)
+print(x)
+
+# Удалить элемент из списка
+del x[1]
+print(x, len(x))
+
+# Добавить
+x[1] = '2'
+print(x, len(x))
+
+#Итерация
+for item in x:
+    item =+ 2
+    print(item)
+    
+print(x)
