13259dabc8c460d3ccb3deca81002e8724633b66
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-22 16:48:45 +0600

    set.py

--- python1/python/set.py
+++ python1/python/set.py
@@ -6,3 +6,15 @@ print(len(f_set))
 f_set = {"alex", "john", "azart", "azart"}
 for user in f_set:
     print(user)
+    
+set = {1, 2, 3, 4, 0, 0, 'str', 6.40, 9j, 4, True}
+print(set)
+
+set = {1, 2, 3, 4, 0, 0, 'str', 6.40, 9j, 4, True}
+print(1 in set)
+
+fruits = {'banana', 'orange', 'apple'} 
+fruits.add('cherry')
+print(fruits)
+
+
