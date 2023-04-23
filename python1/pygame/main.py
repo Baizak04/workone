438e04963ccb8614872e07cc6037f8e76892d2cc
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-23 11:55:38 +0600

    game day-2 the end

--- python1/pygame/main.py
+++ python1/pygame/main.py
@@ -3,10 +3,19 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption("Baizak Game")
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
 
+square = pygame.Surface((50, 170))
+square.fill('red')
 
+myfont = pygame.font.Font()
 running = True
 while running:
+    
+    pygame.draw.circle(square, 'Red', (10, 10), 10)
+    screen.blit(square, (10, 0))
+        
     
     pygame.display.update
     
@@ -14,5 +23,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+      
     
     
