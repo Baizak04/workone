4a8f5faef626d15eff243fb97c6465ee57cf6802
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-26 19:20:12 +0600

    whatsapp

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import pytwhatkit
+import os
+
+def send_message_inst():
+    mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+   
+    pytwhatkit.sendwhatmsg(phone_no=mobile, message=message)
+    
+def send_message():
+    mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+    hour = int(input('Введите часы: '))
+    minutes = int(input('Введите минуты: '))
+    
+    pytwhatkit.sendwhatmsg(phone_no=mobile, message=message, time_hour=hour, time_main=minutes)
+    
+def main():
+    send_message()
+    
+if __name__ == '__name__':
+    main()
