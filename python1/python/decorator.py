ce8e98896a081b721134393c46c350fed76117c8
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-26 13:12:53 +0600

    video-3 decoreator

--- python1/python/decorator.py
+++ python1/python/decorator.py
@@ -19,21 +19,51 @@ print(div(1, 2))
 
 
 
-
-def second_outer(*dargs, **dkwargs):
-    def outer(func):
-        def inner(*args, **kwargs):
+# 2)
+def second_outer(*dargs, **dkwargs):   # Параметры декоратора
+    def outer(func):                   # Сама декорируемая функция  
+        def inner(*args, **kwargs):    # Параметры функции
             attempts = dkwargs["attempts"]
             while attempts > 0:
                 try:
                     return func(*args, **kwargs)
                 except Exception as err:
-                    print(f"Error: {err}, attempts left: {attempts}")
+                    print(f"Ошибка: {err}, осталось попыток: {attempts}")
                     attempts -= 1
         return inner
     return  outer
 
-@second_outer(attempts = 50)
+@second_outer(attempts = 70)
+def div(a, b):
+    return a / b
+
+print(div(1, 0))
+
+
+# 3)
+def second_outer(*dargs, **dkwargs):   # Параметры декоратора
+    def outer(func):                   # Сама декорируемая функция  
+        def inner(*args, **kwargs):    # Параметры функции
+            attempts = dkwargs["attempts"]
+            while attempts > 0:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as err:
+                    print(f"Ошибка: {err}, осталось попыток: {attempts}")
+                    attempts -= 1
+        return inner
+    return  outer
+
+def simple_deco(func):
+    def inner(*args, **kwargs):
+        print("Реклама")
+        return func(*args, **kwargs)
+    return inner
+
+
+@second_outer(attempts = 7)
+@simple_deco
+
 def div(a, b):
     return a / b
 
