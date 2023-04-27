1a3b4581fdfbe9a5ece83c25f9b58c02749da85d
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-27 20:02:09 +0600

    def

--- python1/python/functions.py
+++ python1/python/functions.py
@@ -7,80 +7,83 @@
 
 # Синтаксис для объявления функции:
 
-# def function_name(arguments):
-#     # function body 
+# # def function_name(arguments):
+# #     # function body 
 
-#     return
+# # def имя(параметр):
+#     # блок кода
+
+# #     return
 
 
 
-def greet():
-    print('hello world')
+# def greet():
+#     print('hello world')
     
-greet()
+# greet()
 
 
-def greet(name):
-    result = f'hello {name}'
-    print(result) 
+# def greet(name):
+#     result = f'hello {name}'
+#     print(result) 
        
-greet('world')
-greet('Azat')
+# greet('world')
+# greet('Azat')
 
-# если не передат аргумент или больше то ошибка
+# # если не передат аргумент или больше то ошибка
 
 
-def greet_one(message, name='world'):
-    result = f'{message}, {name}'
-    print(result)
+# def greet_one(message, name='world'):
+#     result = f'{message}, {name}'
+#     print(result)
     
-greet_one(message='Hello')
+# greet_one(message='Hello')
 
 
 
-def greet_two(message, name='world'):
-    result = f'{message}, {name}'
-    return result
+# def greet_two(message, name='world'):
+#     result = f'{message}, {name}'
+#     return result
 
-g = greet_two('hello').title()
-print(g)
-
-
-
-children = ['arbuzov_1000', 'ivanov_2030', 'petrov_1255', 'Azat_2002']
-
-def year(name):
-    return name.split('_')[-1]
+# g = greet_two('hello').title()
+# print(g)
 
 
-s_children = sorted(children, key=year)
-print(s_children)
+
+# children = ['arbuzov_1000', 'ivanov_2030', 'petrov_1255', 'Azat_2002']
+
+# def year(name):
+#     return name.split('_')[-1]
 
 
-def add(x,y):
-    return x + y
-
-print(add('gweru', 'try'))
+# s_children = sorted(children, key=year)
+# print(s_children)
 
 
-def fact(n):
-    prod = 1
-    for i in range(1, n+1):
-        prod *= i
-    return prod
-print(fact(6))
+# def add(x,y):
+#     return x + y
+
+# print(add('gweru', 'try'))
 
 
-def get_user():
-    name = "alex"
-    age = 27
-    admin = False
-    return name, age, admin
+# def fact(n):
+#     prod = 1
+#     for i in range(1, n+1):
+#         prod *= i
+#     return prod
+# print(fact(6))
 
-user = get_user()
-print(user[0])
-print(user[1])
-print(user[2])
+
+# def get_user():
+#     name = "alex"
+#     age = 27
+#     admin = False
+#     return name, age, admin
+
+# user = get_user()
+# print(user[0])
+# print(user[1])
+# print(user[2])
 
 
 # def personal_details():
@@ -90,8 +93,25 @@ print(user[2])
 
 # personal_details()
 
-def div(a, b):
-    return a / b
+# def div(a, b):
+#     return a / b
 
-my_var = div
-print(type(my_var))
+# my_var = div
+# print(type(my_var))
+
+def line():
+    print('------------')
+    
+    
+def max(num1, num2):
+    if num1 > num2:
+        return num1
+    if num2 > num1:
+        return num2
+    
+line()
+
+str = max(2, 15) 
+print(str)
+
+line()
