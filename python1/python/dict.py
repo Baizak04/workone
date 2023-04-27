b2288280e8df188e1b261e718cdf7e1e69292cd5
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-27 16:12:16 +0600

    dict animal

--- python1/python/dict.py
+++ python1/python/dict.py
@@ -1,14 +1,25 @@
 # словари = {}
+# {ключ1: значение1, ключ2: значение2,}
+
+dict = {'cat': 'кошка', 'dog': 'собака'}
+
+# print(dict['dog'])
+
+for key in dict:
+    print(f'{key} это {dict [key]}')
+    
+
+# person = {'name': 'Azamat', 'surname': 'Tentiew', 'email': 'a@gmail.com'}
+# # print(person)
+
+# d = dict(name='peter', number='123')
+# print(d)
 
 
-person = {'name': 'Azamat', 'surname': 'Tentiew', 'email': 'a@gmail.com'}
-# print(person)
-d = dict(name='peter', number='123')
-print(d)
+# d = dict(name='peter', number='123')
+# name = person['name']
+# print(name)
 
 
-d = dict(name='peter', number='123')
-name = person['name']
-print(name)
 
 
