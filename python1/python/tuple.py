6db14829c32ed25a28053bf551e9e5e19eedddd4
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-28 15:00:15 +0600

    tuple continues

--- python1/python/tuple.py
+++ python1/python/tuple.py
@@ -2,3 +2,22 @@
 # кортеж работает быстрее чем список[list]
 # tuple = ()
 
+cor1 = ("hello", "sorry", 1, 2, 2000)
+cor2 = (1, 2, 2, 3, 4)
+
+print(cor1)
+print(cor2)
+
+
+
+one = (5,)
+print(one)
+
+
+cor_one = ("салам", "name", 18, 2003)
+cor_two = (12, 2, 23, 3)
+a = input("Введите название объекта: ")
+if a in cor_one:
+    print("Нашел!")
+else:
+    print("Не нашел!")
