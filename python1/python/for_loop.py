004decc1dd4e9d57383fe221b7c278e8d41a8455
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-28 12:23:17 +0600

    for world lesson

--- python1/python/for_loop.py
+++ python1/python/for_loop.py
@@ -1,32 +1,32 @@
-# цикл - фрагмент кода, который выполняется многократно
-# цикл (for, while)
-# Итерирование - процесс перебора итерируемого объекта
-# for имя_переменной in имя итерируемого объекта:
-#     инструкции
+# # цикл - фрагмент кода, который выполняется многократно
+# # цикл (for, while)
+# # Итерирование - процесс перебора итерируемого объекта
+# # for имя_переменной in имя итерируемого объекта:
+# #     инструкции
 
-arr = [1, 2, 3, 4, 5]
-for i in arr:
-    print("Число {}".format(i))
+# arr = [1, 2, 3, 4, 5]
+# for i in arr:
+#     print("Число {}".format(i))
 
 
-# children = ['arbuzov_1000', 'ivanov_2030', 'petrov_1255', 'Azat_2002']
+# # children = ['arbuzov_1000', 'ivanov_2030', 'petrov_1255', 'Azat_2002']
 
-# names = []
+# # names = []
 
-# for childrens in children:
-#     surname = childrens.split('_')[0]
-#     surname = surname.title()
-#     print(surname)
+# # for childrens in children:
+# #     surname = childrens.split('_')[0]
+# #     surname = surname.title()
+# #     print(surname)
     
     
-# name = "Привет кто-то!"
-# for i in name:
-#     print(i)
-# # выводит по отдельному
+# # name = "Привет кто-то!"
+# # for i in name:
+# #     print(i)
+# # # выводит по отдельному
 
-# name = "hello"
-# for i in range(1,11):
-#     print(name)
+# # name = "hello"
+# # for i in range(1,11):
+# #     print(name)
     
     
 for j in 'hello world':
@@ -34,7 +34,22 @@ for j in 'hello world':
     
     
 
-for j in 'hello world':
-    if j == 'w':
+# for j in 'hello world':
+#     if j == 'w':
+#         break
+#     print(j * 4, end = '')
+    
+    
+# for i in range(10):
+#     print(i, end=" ")
+    
+    
+# else - lesson
+
+for i in 'hello world' :
+    if i=='h':
+        print(' буквы существует!')
         break
-    print(j * 4, end = '')
+    else:
+        print(' Буквы в строке нету')
+        break
