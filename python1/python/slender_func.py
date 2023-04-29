a505fe0c0abc28b9a34b191484270131c7130c90
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-29 19:27:43 +0600

    slender_func.py

---
+++ python1/python/slender_func.py
@@ -0,0 +1,19 @@
+# a = abs(-5.5)
+# print(a)
+
+# b = chr(98)
+# print(b)
+
+# c = help(print)
+# print(c)
+
+
+d = 122,223,334,13
+print(min(d))
+
+f = 122,223,334,13
+print(max(f))
+
+g = "салам"
+print(list(g))
+
