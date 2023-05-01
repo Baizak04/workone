9c0a9361061219b483c313c78481b6daecad4029
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-01 19:28:14 +0600

    tkinter buttons

---
+++ tkinter/tkinter_test.py
@@ -0,0 +1,25 @@
+# кнопки tkinter
+
+# var = IntVar()
+
+# var.set(1)
+# r1 = Radiobutton(root, text="Windows", variable=var, value=0)
+# r2 = Radiobutton(root, text="Linux", variable=var, value=1)
+
+# c1 = IntVar()
+# c2 = IntVar()
+
+# che1 = Checkbutton(root, text="первый флажок", variable=c1, offvalue=0)
+# che2 = Checkbutton(root, text="второй флажок", variable=c2, offvalue=0)
+
+
+# # but.pack()
+# # ent.pack()
+# # lab.pack()
+# # text.pack()
+# r1.pack()
+# r2.pack()
+# che1.pack()
+# che2.pack()
+
+# root.mainloop()
