fe55593b7ddc1dc2aff7602708258a94b9aeac42
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-02 15:29:46 +0600

    scale-tkinter

--- tkinter/tkinter_buttons.py
+++ tkinter/tkinter_buttons.py
@@ -16,15 +16,26 @@ root.geometry("800x400")     # начальные размеры окна
 
 # widget text 
 
-fra1 = Frame(root, width=400, height=150, bg="darkred")
-fra2 = Frame(root, width=250, height=100, bg="green", bd=20)
-fra3 = Frame(root, width=100, height=50, bg="darkblue")
+# fra1 = Frame(root, width=400, height=150, bg="darkred")
+# fra2 = Frame(root, width=250, height=100, bg="green", bd=20)
+# fra3 = Frame(root, width=100, height=50, bg="darkblue")
 
-ent1 = Entry(fra1, width=20,)
+# ent1 = Entry(fra1, width=20,)
 
-fra1.pack()
-fra2.pack()
-fra3.pack()
-ent1.pack()
+# fra1.pack()
+# fra2.pack()
+# fra3.pack()
+# ent1.pack()
+
+# шкала
+
+fra = Frame(root, width=200, height=50, bg="darkblue")
+
+sca1 =Scale(fra, orient=HORIZONTAL, length=300, from_=0, to=10, tickinterval=0.1, resolution=0.1)
+sca2 =Scale(root, orient=VERTICAL, length=400, from_=1, to=2, tickinterval=0.1, resolution=0.1)
+
+fra.pack()
+sca1.pack()
+sca2.pack()
 
 root.mainloop()
--- tkinter/tkinter_two.py
+++ tkinter/tkinter_two.py
@@ -4,26 +4,43 @@ root = Tk()
 root.title("Программа")      # Заголовок окна
 root.geometry("800x400")     # начальные размеры окна
 
-but = Button(root,
-             text="нажми",
-             width=20, height=2,
-             bg="white", fg="blue")
+# but = Button(root,
+#              text="нажми",
+#              width=20, height=2,
+#              bg="white", fg="blue")
 
-lab = Label(root, text="что-то", font="Arial 16")
+# lab = Label(root, text="что-то", font="Arial 16")
 
-ent = Entry(root, width=15, bd=2)
+# ent = Entry(root, width=15, bd=2)
 
-text = Text(root, width=15, font="Oswald 16", wrap=WORD)
+# text = Text(root, width=15, font="Oswald 16", wrap=WORD)
 
-r1 = Radiobutton(text="Windows")
-r2 = Radiobutton(text="Linux")
+# var = IntVar()
+
+# var.set(1)
+# r1 = Radiobutton(root, text="Windows", variable=var, value=0)
+# r2 = Radiobutton(root, text="Linux", variable=var, value=1)
+
+# c1 = IntVar()
+# c2 = IntVar()
+
+# che1 = Checkbutton(root, text="первый флажок", variable=c1, offvalue=0)
+# che2 = Checkbutton(root, text="второй флажок", variable=c2, offvalue=0)
 
 
-but.pack()
-ent.pack()
-lab.pack()
-text.pack()
-r1.pack()
-r2.pack()
+# # but.pack()
+# # ent.pack()
+# # lab.pack()
+# # text.pack()
+# r1.pack()
+# r2.pack()
+# che1.pack()
+# che2.pack()
 
+r = ['Python', 'C#', 'C++', 'java', 'C+']
+lis = Listbox(root, selectmode=SINGLE, height=5)
+for i in r:
+    lis.insert(END, i)
+    
+lis.pack()
 root.mainloop()
