dbce389f389854cf2fbc4e0c37d9ac59ecea0957
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-03 20:10:21 +0600

    def *args and **kwargs

--- python2/def.py
+++ python2/def.py
@@ -11,5 +11,23 @@ result = add(20, 30)
 print(result)
 
 
+def myfunc(*args):
+    return sum(args) * 2
+
+result = myfunc(30, 23, 34, 67, 30, 12, 23)
+print(result)
+
+def myfunc(*args):
+    for i in args:
+        print(i)
+
+myfunc(30, 23, 34, 67, 30, 12, 23)
 
 
+def myfunc_two(**kwargs):
+    if 'fruit' in kwargs:
+        print(f'My fruit of choice is {kwargs}')
+    else:
+        print('Я не нашол такой слово')
+        
+myfunc_two(fruit='apple')
