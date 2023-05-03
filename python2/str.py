43d1dbb0ba8f48e1d6f024a37ed878b545d51e83
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-03 14:22:42 +0600

    basic str.py

---
+++ python2/str.py
@@ -0,0 +1,23 @@
+# mystring = 'Hello World'
+# print(mystring[-3])
+
+
+# mystring_one = 'asdfghjkl'
+# print(mystring_one[2:6])
+
+# a = "sum"
+# a = 'd' + a
+# print(a)
+
+# letter = 's'
+# letter = letter * 10
+# print(letter)
+
+
+# Свойста и методы строк
+
+x = "hello world"
+print(x.upper())
+
+z = "mairambek"
+print(z.title())
