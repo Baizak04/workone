20008dc1b33877a095879813f93e52b06c23df98
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-03 15:58:29 +0600

    basic dict.py

---
+++ python2/dict.py
@@ -0,0 +1,22 @@
+prices = {'apple' : 35, 'oranges' : 3.99, 'milk': 70}
+print(prices['oranges'])
+
+
+d = {'k1' : 123, 'k2': [1, 2, 3, 4]}
+print(d['k2'][3])
+
+d = {'key1' : 100, 'key2' : 200}
+d['key3'] = 300
+print(d)
+
+
+d = {'key1' : 100, 'key2' : 200, 'key3' : 300}
+print(d.keys())
+
+d = {'key1' : 100, 'key2' : 200, 'key3' : 300}
+print(d.values())
+
+d = {'key1' : 100, 'key2' : 200, 'key3' : 300}
+print(d.items())
+
+
