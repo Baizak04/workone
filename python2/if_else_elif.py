da5e395e2233e60c253378a9ce95a7eb7a14157a
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-03 18:56:25 +0600

    basic if_else_elif.py

---
+++ python2/if_else_elif.py
@@ -0,0 +1,26 @@
+hungry = True
+if hungry:
+    print("ПОКОРМИ МЕНЯ!")
+else:
+    print("Я не голоден")
+    
+    
+loc = 'Auto'
+
+if loc == 'Auto':
+    print("машины крутые")
+else:
+    print("я мало что знаю")
+    
+    
+    
+name = 'Sammy'
+
+if name == 'Frankie':
+    print("Hello Frankie")
+elif name == 'Sammy':
+    print("Heloo Sammy")
+else:
+    print("What is your name?")
+
+    
