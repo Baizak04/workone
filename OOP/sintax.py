56f3f2b17078600c057b81cbbb26dcf6d4bf7dae
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-05 19:01:35 +0600

    suntax OOP

---
+++ OOP/sintax.py
@@ -0,0 +1,10 @@
+class NameOfClass():
+    
+    def __init__(self, param1, param2): # это методы похоже функции
+        self.param1 = param1
+        self.param2 = param2
+        
+    def some_method(self):
+        # выполнить некоторое действие
+        print(self.param1)
+        
