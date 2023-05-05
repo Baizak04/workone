830fe5fe86e7f3fa340bf93390c4fae81e8f099c
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-05 19:31:35 +0600

    dog continue

--- OOP/oop1.py
+++ OOP/oop1.py
@@ -16,11 +16,14 @@ except TypeError:
     
     
 class Dog():
-    
-    def __init__(self, breed):
+    spec = 'mammal'
+    def __init__(self, breed, name, spots):
         
         self.breed =  breed
+        self.name = name
+        self.spots = spots
         
-my_dog = Dog(breed='a')
+my_dog = Dog(breed='a', name='sam', spots=False)
+
 # print(my_dog)
-print(my_dog.breed)
+print(my_dog.breed, my_dog.name, my_dog.spots)
