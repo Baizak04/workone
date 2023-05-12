d481fb64349d6d867c2075f66513edd6e0af3b34
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-12 16:48:43 +0600

    task

---
+++ python2/python_today.py
@@ -0,0 +1,39 @@
+# from getpass import getpass
+
+# # 1)Скрыть пароль на Python
+# username = input('Введите имя: ')
+# password = getpass('Введите пароль: ')
+
+
+
+
+# try:
+#     x = int(input())
+#     y = int(input())
+
+#     if x > 0 and y > 0:
+#         print(1)
+#     else:
+#         if x < 0 and y > 0:
+#             print(2)
+#         else:
+#             if x < 0 and y <   0:
+#                 print(3) 
+#             else:
+#                 if x > 0 and y < 0:
+#                     print(4)
+#                 else:
+#                     print("НИКОГДА!")  
+# except ValueError:
+#     print('ошибка')
+                
+                
+# def sum(a=2, b=2):
+#     print(a + b)
+    
+# sum(2)
+
+start = 10
+end = 0
+step = -1
+ 
