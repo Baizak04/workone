8894efcb5da370eba7aaf40d400785715c9a3507
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-13 13:59:38 +0600

    math

--- python2/python_today.py
+++ python2/python_today.py
@@ -1,4 +1,5 @@
 # from getpass import getpass
+from math import *
 
 # # 1)Скрыть пароль на Python
 # username = input('Введите имя: ')
@@ -33,7 +34,33 @@
     
 # sum(2)
 
-start = 10
-end = 0
-step = -1
+# start = 10
+# end = 0
+# step = -1
  
+# for i in range(start, end, step):
+#     print(i, end=' ')
+
+
+# number = input()
+# ansver = 0
+
+# for i in number:
+#     ansver += int(i)
+    
+# print(ansver)
+
+AB = input("Длина первого катета: ")
+AC = input("Длина второго катета: ")
+
+AB = float(AB)
+AC = float(AC)
+
+BC = sqrt(AB ** 2 + AC ** 2)
+
+S = (AB * AC) / 2
+
+P = AB + AC + BC
+
+print("Площадь треугольника ", S)
+print("Периметр треугольника ", P)
