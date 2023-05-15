4d7cd96070a8eee260217c0374cfa38137f74401
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-15 16:19:22 +0600

    for.py example

--- python2/for.py
+++ python2/for.py
@@ -1,18 +1,22 @@
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for num in mylist:
-    if num % 2 == 0:
-        print(f'четное число: {num}')
-    else:
-        print(f'нечетное число: {num}')
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for num in mylist:
+#     if num % 2 == 0:
+#         print(f'четное число: {num}')
+#     else:
+#         print(f'нечетное число: {num}')
     
     
-for letter in 'Hello World':
-    print(letter)
+# for letter in 'Hello World':
+#     print(letter)
     
-mylist = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
-for i in mylist:
-    print(i)
+# mylist = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
+# for i in mylist:
+#     print(i)
     
-d = {'k1': 1, 'k2': 2, 'k3': 3}
-for i in d.items():
-    print(i)
+# d = {'k1': 1, 'k2': 2, 'k3': 3}
+# for i in d.items():
+#     print(i)
+    
+for i in range(4):
+    print(i, i**2)
+print("Цикл закончился")
