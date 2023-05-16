67003687b2122bcd0752d57ccde757aad9795e7c
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-16 19:20:52 +0600

    basic game

---
+++ game/lol.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+
+pygame.init()
+
+gamewindow = pygame.display.set_mode((320, 240))
+
+WHITE = (255, 255, 255)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            
+    gamewindow.fill(WHITE)
+    pygame.display.flip
