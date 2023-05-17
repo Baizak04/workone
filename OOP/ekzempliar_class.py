0c65fd5739df85c3dc6eabb88ce634c866f4c287
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-17 18:02:57 +0600

    practic_two

--- OOP/ekzempliar_class.py
+++ OOP/ekzempliar_class.py
@@ -13,3 +13,42 @@ person_two.hair_color = 'white'
 person_one.height = 165
 
 print(person_one.__dict__, person_two.__dict__, Person.__dict__, sep='\n')
+
+
+# practic
+
+class Person:
+    age: int = 20
+    height: int = 170
+    
+    
+person_one = Person()
+person_two = Person()
+
+person_one.height = 160
+print(person_one.__dict__)
+
+person_two.hair_color = 'yellow'
+print(person_two.__dict__)
+
+print(Person.__dict__)
+
+
+
+class Cat:
+    age: int = 2
+    breed: str = 'Bengal'
+    
+tom = Cat()
+setattr(tom, 'age', 1)
+print(getattr(Cat, 'breed'), getattr(Cat, 'age'), sep='\n')
+print()
+print(tom.__dict__, tom.age)
+
+
+class Car:
+    speed: float = 90.0
+    color: str = 'red'
+    
+    
+print(Car.speed, Car.color, sep='\n')
