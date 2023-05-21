0f22dcfc444f7801b763b4f55beb97d721bca0b6
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-21 21:11:30 +0600

    game telebot

---
+++ baizak04_bot/bot.py
@@ -0,0 +1,101 @@
+import telebot
+
+bot = telebot.TeleBot('6226915086:AAEN1Sr9j2RJnfrZrGXZKBkdEQwPQ7LoOEY')
+
+@bot.message_handler(commands=['starttwo'])
+def starttwo(message):
+    bot.send_message(message.chat.id,  'Привет Я ваш персональный бот 😊 Чем могу помочь')
+    
+@bot.message_handler(commands=['id'])
+def starttwo(message):
+    bot.send_message(message.chat.id, message.chat.id)
+    
+    
+@bot.message_handler(commands=['Псевдоним'])
+def starttwo(message):
+    bot.send_message(message.chat.id, str(message.from_user.username))
+     
+
+    
+@bot.message_handler(commands=['Имя'])
+def starttwo(message):
+    bot.send_message(message.chat.id, str(message.from_user.first_name))
+     
+def salam(message):
+    return message.text == "Салам алейкум"
+
+@bot.message_handler(func=salam)
+def start(message):
+    bot.reply_to(message, 'Ваалейкум салам. Мой хозяин занят на данный момент')
+
+
+def salam(message):
+    return message.text == "Привет"
+
+@bot.message_handler(func=salam)
+def start(message):
+    bot.reply_to(message, 'Привет дорогуша')
+
+def jan(message):
+    return message.text == "Жан"
+
+@bot.message_handler(func=jan)
+def jan(message):
+    bot.reply_to(message, 'оуу жаным угуп жатам 😊')
+    
+def good_morning(message):
+    return message.text == "Доброе утро жан"
+
+@bot.message_handler(func=good_morning)
+def good_morning(message):
+    bot.reply_to(message, 'Доброе ❤️')
+    
+
+def what(message):
+    return message.text == "Чем ты занимаешься?"
+
+@bot.message_handler(func=what)
+def what(message):
+    bot.reply_to(message, 'Лежу \n думаю что-то сделать с тобой ')
+    
+    
+def hwo(message):
+    return message.text == "Кто ты?"
+
+@bot.message_handler(func=hwo)
+def hwo(message):
+    bot.reply_to(message, 'Я помощьник хозяина Байзака ')
+    
+    
+def hwo(message):
+    return message.text == "Ты моё солнце?"
+
+@bot.message_handler(func=hwo)
+def hwo(message):
+    bot.reply_to(message, '<strong>Нет</strong>', parse_mode='html')
+    
+    
+@bot.message_handler(content_types=['photo'])
+def photo_one(message):
+    bot.send_message(message.chat.id, 'Вауу красивое фото 🤩')
+
+
+@bot.message_handler(content_types=['voice'])
+def audio_one(message):
+    bot.send_message(message.chat.id, 'у тебя голос как девчатое 🤭')
+
+
+@bot.message_handler(content_types=['video'])
+def video_one(message):
+    bot.send_message(message.chat.id, 'щяс... \n загрузка \n ну нормальное видео 😏')
+
+@bot.message_handler(regexp=r'[0-9]+')
+def sum_one(message):
+    bot.send_message(message.chat.id, 'Ты что шитать не умеешь \n Научис шитать')
+
+@bot.message_handler(commands=['itspace'])
+def itspace(message):
+    bot.send_message(message.chat.id, '<i>itspace</i>' ,parse_mode='html')
+     
+
+bot.polling()
