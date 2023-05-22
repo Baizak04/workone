10dd0bb3dbd3790666a003cc058382d1b6c0dabb
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-22 11:31:51 +0600

    callback bot

--- baizak04_bot/bot.py
+++ baizak04_bot/bot.py
@@ -149,7 +149,26 @@ def switch(message):
     kb.add(switch)
     bot.send_message(message.chat.id, 'Сообщение', reply_markup=kb)
 
+@bot.message_handler(commands=['callback'])
+def callback(message):
+    kb = types.InlineKeyboardMarkup(row_width=1)
+    btn = types.InlineKeyboardButton(text='Салам', callback_data='btn1')
+    btn1 = types.InlineKeyboardButton(text='Ваалейкум', callback_data='btn2')
+    kb.add(btn, btn1)
+    bot.send_message(message.chat.id, 'выбери что-то из них', reply_markup=kb)
+    
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def check_callback_data(callback):
+    if callback.data == 'btn1':
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        btn = types.InlineKeyboardButton(text='алейкум', callback_data='btn2')
+        btn1 = types.InlineKeyboardButton(text='салам', callback_data='btn1')
+        kb.add(btn, btn1)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='хихих 😂 \n Предупреждения!!! дальше ничего не нажимай', reply_markup=kb)    
 
+
+
+bot.polling()
 
 
 bot.polling()
