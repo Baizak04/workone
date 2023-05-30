27a333aba0c78ad55e4a78d1116d18485a70a83e
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-30 14:00:26 +0600

    format.py

--- python3/basic.py
+++ python3/basic.py
@@ -1,18 +1,18 @@
-# name = input('Введите свое имя: ')
-# first_name = input('Введите свое фамилию: ')
-# age = int(input('Сколько вам лет?: '))
-# income = float(input('Какой у вас доход?: '))
-# print('Вот данные которые Вы ввели: ')
-# print('Имя:', name)
-# print('Возраст:', age)
-# print('Фамилия:', first_name)
-# print('Доход:', income)
+name = input('Введите свое имя: ')
+first_name = input('Введите свое фамилию: ')
+age = int(input('Сколько вам лет?: '))
+income = float(input('Какой у вас доход?: '))
+print('Вот данные которые Вы ввели: ')
+print('Имя:', name)
+print('Возраст:', age)
+print('Фамилия:', first_name)
+print('Доход:', income)
 
 
-# salary = 12000
-# bonus = 1000
-# pay = salary + bonus
-# print('Ваша заработная плата составляет', pay)
+salary = 12000
+bonus = 1000
+pay = salary + bonus
+print('Ваша заработная плата составляет', pay)
 
 
 original_price = float(input("Введите исходную цену товара: "))
---
+++ python3/format.py
@@ -0,0 +1,22 @@
+# 1)
+temperature = 45
+print(f'Сейчас {temperature} градусов')
+
+# 2)
+print(f'Значение ровно {10 + 2}')
+
+# 3)
+pi = 3.23232
+print(f'{pi: 3f}')
+
+# 4)
+number = 99
+print(f'Число равняется {number:10}')
+
+# 5)
+name = 'Канат'
+department = 'Отдел продажи'
+position = 'Менеджер'
+print(f'Имя {name}\n',  
+      f'Отдел {department}\n',
+      f'Позиция {position}')
