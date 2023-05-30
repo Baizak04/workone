b3f96b3c51cf2b3f6a6fbab060f9cabdd3a1ef41
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-30 11:56:22 +0600

    basic python

---
+++ python3/basic.py
@@ -0,0 +1,21 @@
+# name = input('Введите свое имя: ')
+# first_name = input('Введите свое фамилию: ')
+# age = int(input('Сколько вам лет?: '))
+# income = float(input('Какой у вас доход?: '))
+# print('Вот данные которые Вы ввели: ')
+# print('Имя:', name)
+# print('Возраст:', age)
+# print('Фамилия:', first_name)
+# print('Доход:', income)
+
+
+# salary = 12000
+# bonus = 1000
+# pay = salary + bonus
+# print('Ваша заработная плата составляет', pay)
+
+
+original_price = float(input("Введите исходную цену товара: "))
+discount = original_price * 2.2
+sala_price = original_price - discount
+print('Отпускная цена составляет', sala_price)
