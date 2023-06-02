ad63aca1be8cdee3ae92f06455bc5ae847addb0c
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-02 19:57:10 +0600

    two function

--- python3/finctions.py
+++ python3/finctions.py
@@ -8,3 +8,29 @@ def message():
     print('Президент')
     
 main()
+
+
+# 2)
+
+def main():
+    start()
+    input('Нажмите на Enter увидете шаг1')
+    step()
+    input('Нажмите на Enter увидете шаг2')
+    step1()
+    input('Нажмите на Enter увидете шаг3')
+    step2()
+    
+def start():
+    print('Что-то')
+    
+def step():
+    print('Это шаг1')
+    
+def step1():
+    print('Это шаг2')
+    
+def step2():
+    print('Это шаг3')
+    
+main()
