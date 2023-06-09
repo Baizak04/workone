d141ca6e4588664ceafb1f8edc4e597e4d935a74
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-09 16:48:05 +0600

    basic set.py

---
+++ python-10hours/set.py
@@ -0,0 +1,10 @@
+my_set = {10, 12, 5, 5, 5, 5, 35}
+print(my_set)
+print(len(my_set))
+
+my_set = {(10, 10), 10, 4, 22}
+print(my_set)
+
+my_two_set = set()
+print(my_two_set)
+print(type(my_two_set))
