b91142a402872df0ed018554d143cb1a66ddb541
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-09 18:01:15 +0600

    set.py

--- python-10hours/set.py
+++ python-10hours/set.py
@@ -8,3 +8,14 @@ print(my_set)
 my_two_set = set()
 print(my_two_set)
 print(type(my_two_set))
+
+# 2)
+
+my_set = {'abs', 'd', 'f', 'y'}
+other_set = {'f', 'd'}
+print(my_set.intersection('absd'))
+print(my_set.union(other_set))
+print(other_set.issubset(my_set))
+print(my_set.difference(other_set))
+print(my_set & other_set)
+print(my_set | other_set)
