a807f1f3a90fdf24cff3c680c3682f48e5564f17
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-09 21:03:42 +0600

    add 10code task.py

--- python3/python3/task.py
+++ python3/python3/task.py
@@ -55,3 +55,70 @@ print(result)
 # 10)
 world = 'you are doing well'
 print(world[2:999])
+
+# 11)
+for i in range(0):
+    print(i)
+    
+# 12)
+# dictionary = {
+#   "data": "user",
+#   "user": 7,
+#   "7": "data"
+# }
+
+# print(dictionary[dictionary["user"]]) # KeyError
+
+# 13)
+
+name = "James Bond"
+print(name[2::-1])
+
+# 14)
+tuple_one = (1, 'Jon', "3+j")
+print(type(tuple_one[2:3]))
+
+# 15) 
+text_hello = "Hello world!"
+text_hello.replace("o", "-")
+print(text_hello)
+
+# 16)
+def outer_fun(a, b):
+    def inner_fun(c, d):
+        return c + d
+    return inner_fun(a ,b)
+    return a
+
+result = outer_fun(5, 10)
+print(result)
+
+# 17)
+sample_set = {"yellow", "orange", "black"}
+sample_set.discard("blue")
+print(sample_set)
+
+# 18)
+var = 10
+for i in range(10):
+    for j in range(2, 10, 1):
+        if var % 2 == 0:
+            continue
+            var += 1
+    var += 1
+else:
+    var += 1
+print(var)
+
+# 19)
+for num in range(10, 14):
+    for i in range(2, num):
+        if num % i == 1:
+            print(num)
+            break
+        
+# 20)
+for l in 'Jhon':
+    if l == 'o':
+        pass
+    print(l, end=", ")
