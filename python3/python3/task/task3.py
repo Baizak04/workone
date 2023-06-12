b2e3ddb0fd7f0245fd49e36c58d7445126caa9ec
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-12 18:00:25 +0600

    add 6 code new file task3.py

---
+++ python3/python3/task/task3.py
@@ -0,0 +1,54 @@
+# 1)
+def f1():
+    x = 15
+    print(x)
+      
+      
+x =12
+f1()
+
+# 2)
+# i = 1
+# while True:
+#     if i%2 == 0:
+#         break
+    
+#     print(i)
+    
+#     i += 2
+    
+    
+# 3)
+def foo(k):
+    k = [1]
+    
+    
+q = [0]
+foo(q)
+print(q)
+
+# 4)
+def f1():
+    x = 100
+    print(x)
+    
+    
+    
+x = +1
+f1()
+
+# 5)
+i = 2
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+    i += 2
+    
+# 6)
+d = {"john":40, "peter":45}
+print(list(d.keys()))
+    
+
+    
+    
