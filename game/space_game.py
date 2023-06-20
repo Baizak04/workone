61c636c2f1d548a4794149da49edb20562da10ff
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-20 21:36:59 +0600

    Creating an alien army

--- game/space_game.py
+++ game/space_game.py
@@ -2,21 +2,23 @@ import pygame, controls
 from gun import Gun
 from pygame.sprite import Group
 
-def run():
-    
+
+def run():    
     pygame.init()
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((700, 800))
     pygame.display.set_caption("Космические защитники")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
     bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
     
     
     while True:
         controls.events(screen, gun, bullets) 
         gun.update_gun()   
-        controls.update(bg_color, screen, gun, bullets)
+        controls.update(bg_color, screen, gun, inos, bullets)
         controls.update_bullets(bullets)
-    
+        controls.update_inoc(inos)
     
 run()
