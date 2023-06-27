aa84f474a460bf9529238075aef335a55142992d
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-27 17:29:36 +0600

    basic str.py

---
+++ python of course presention/str.py
@@ -0,0 +1,25 @@
+# 1)
+mystring = 'Hello World'
+print(mystring[0])
+
+# 2)
+mystring = 'Hello World'
+print(mystring[-3])
+
+# 3)
+mystring_one = 'asdfghjkl'
+print(mystring_one[2:])
+print(mystring_one[2:6])
+
+# 4)
+animal = 'catdog'
+print(animal[2:4])
+
+# 6)
+letter = 's'
+letter = letter * 10
+print(letter)
+
+# 6)
+msg = "hello" * 99
+print(msg)
