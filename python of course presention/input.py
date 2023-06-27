9df6acee78aa2db1f4f80c635d8e070bc3ffb427
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-27 17:49:32 +0600

    add input.py

---
+++ python of course presention/input.py
@@ -0,0 +1,6 @@
+print("***ДОБРО ПОЖАЛОВАТЬ В ПРИЛОЖЕНИЕ ИМЯ***")
+first_name = input("Как вас зовут? : ")
+last_name = input("Какая у вас фамилия? : ")
+
+full_name = first_name + " " + last_name
+print("Здравствуйте, " + full_name)
