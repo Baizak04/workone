4f062535f4bd3897ff31eb87c7cf2cdaff928c0e
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-27 18:43:52 +0600

    add code type.py

---
+++ python of course presention/type.py
@@ -0,0 +1,26 @@
+
+# 1)
+a = float(23)
+print(a)
+
+# 2)
+b = int(23.5)
+print(b)
+
+# 3)
+print(type("hi"))
+
+# 4)
+print(type(23))
+
+# 5)
+print(type(24.0))
+
+# 6)
+days = 365.25 * int(input("Сколько тебе лет? "))
+days = str(days)
+print("Вы прожили " + days, " " + "дней")
+
+# 7)
+days = 365.25 * int(input("Сколько лет вы прожили? "))
+print(f"Вы прожили {days} дней")
