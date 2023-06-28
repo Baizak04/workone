87137d55b2e54974e5bb515ef356c631f1140652
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-28 14:18:15 +0600

    add code while

---
+++ python of course presention/while.py
@@ -0,0 +1,19 @@
+# # 1)
+# answer = input("Пожалуйста скажите привет ")
+
+# while answer != "привет":
+#     answer = input("Грубо. Скажи привет... ")
+    
+# print("Спасибо. И вам привет!")
+
+# # 2)
+# number = 10
+# while number > 0:
+#     print(f"ЧИСЛО ЕСТЬ: {number}")
+#     number -= 1
+    
+# # 3) 
+count = 7
+while count > 0:
+    print("*" * count)
+    count -= 1
