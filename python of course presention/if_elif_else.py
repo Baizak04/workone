596327d92a01798a9495e031f87fc4aefdf4975c
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-28 13:51:28 +0600

    add code if_elif_else.py

---
+++ python of course presention/if_elif_else.py
@@ -0,0 +1,28 @@
+# 1)
+age = 21
+if age >= 21:
+    print("Добро пожаловать")
+else:
+    print("Иди домой, малыш.")
+    
+# 2)    
+color = "Цвет зеленый"
+if color == "черный":
+    print("Не тот цвет")
+elif color == "зеленый":
+    print("У нас тоже зеленый")
+else:
+    print("Такого цвета нету")
+    
+# 3)
+english_level = str(input("Какой ваш уровень? "))
+
+if english_level == "A1":
+    print("BEGINNER")
+elif english_level == "B1":
+    print("INTERMEDIATE")
+elif english_level == "C1":
+    print("ADVANCED")
+else:
+    print("ВЫ неправильно указали")
+    
