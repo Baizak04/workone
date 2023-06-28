04b0722cec1491e3b913482ae4d0041a494fbbfc
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-28 21:05:06 +0600

    basic raise.py

---
+++ python of course presention/raise.py
@@ -0,0 +1,9 @@
+# def get_user_name():
+#     inp = input('Введите ваше имя: ')
+#     if not inp.isalpha():
+#         raise ValueError('Что-то')
+#     return inp
+
+# def register_user():
+#     user = get_user_name()
+#     Database.save(user)
