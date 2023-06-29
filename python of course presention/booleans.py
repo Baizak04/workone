a805ed681225874c6743be349c604d6c9bc8ba00
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-29 18:00:56 +0600

    add code true and false

--- python of course presention/booleans.py
+++ python of course presention/booleans.py
@@ -7,4 +7,18 @@ print(0 <= -99)
 # 3)
 print("A" in "abda")
 
+# 4)
 print("A" in "Aleks")
+
+# 5)
+print(bool(10))
+print(bool('abc'))
+print(bool([]))
+print(bool([1, 2]))
+print(bool(None))
+
+# 6)
+print(100 > 10)
+print('Long string' > 'long')
+print([1, 2, 3] == [1, 2, 3])
+print({'a': 3} == {'a': 5})
