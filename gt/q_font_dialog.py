2c92652b1da2fd4c4af1c2c95b2330f98c201965
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-07-10 13:24:12 +0600

    add code q_font_dialog

---
+++ gt/q_font_dialog.py
@@ -0,0 +1,35 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class DlgMain(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.resize(200, 200)
+
+        self.btn = QPushButton('Choose Font', self)
+        self.btn.move(35, 50)
+        font = QFont('Arial', 14, 76, True)
+        self.btn.setFont(font)
+        self.btn.clicked.connect(self.evt_btn_clicked)
+
+    def evt_btn_clicked(self):
+        font, b_ok = QFontDialog.getFont()
+        print(font, b_ok)
+        if b_ok:
+            print(font.family())
+            print(font.italic())
+            print(font.bold())
+            print(font.weight())
+            print(font.pointSize())
+            self.btn.setFont(font)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
