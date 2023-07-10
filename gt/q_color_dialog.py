fa71bd68b8f87c388975678a7477b31e5688163b
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-07-10 13:07:54 +0600

    add code q_color_dialog

---
+++ gt/q_color_dialog.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class DlgMain(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.resize(200, 200)
+
+        self.btn = QPushButton('Choose Color', self)
+        self.btn.move(35, 50)
+        self.btn.clicked.connect(self.evt_btn_clicked)
+
+    def evt_btn_clicked(self):
+        color = QColorDialog.getColor(QColor('#FF0000'), self, 'Choose color')
+        print(color)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
