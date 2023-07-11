1804eaf035284bf4758d48936297dc0b8e92949f
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-07-11 17:21:51 +0600

    add code

--- pygame/test.py
+++ pygame/test.py
@@ -1,13 +1,17 @@
 import pygame as pg
 
 pg.init()
-sc = pg.display.set_mode((800, 500))
+sc = pg.display.set_mode((800, 450), pg.FULLSCREEN | pg.SCALED)
+clock = pg.time.Clock()
+
+FPS = 60
 
 while True:
+    sc.fill((0, 0, 100))
     for ev in pg.event.get():
         if ev.type == pg.QUIT:
             exit()
 
-    pg.draw.rect(sc, (212, 51, 51), (100, 100, 70, 20))
+    # pg.draw.rect(sc, (212, 51, 51), (100, 100, 70, 20))
 
     pg.display.update()
