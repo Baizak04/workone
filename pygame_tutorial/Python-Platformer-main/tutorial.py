af0d8ad9fd447b89552b9243e594b3c460ced983
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-07-11 18:45:41 +0600

    add code background

--- pygame_tutorial/Python-Platformer-main/tutorial.py
+++ pygame_tutorial/Python-Platformer-main/tutorial.py
@@ -10,7 +10,6 @@ pygame.init()
 
 pygame.display.set_caption("Игра что-то")
 
-BG_COLOR = (252, 252, 252)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
@@ -18,8 +17,29 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tuple(tile))
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("/home/lenovo/python/pygame_tutorial/Python-Platformer-main/assets/Background/Blue.png")
 
     run = True
     while run:
@@ -29,6 +49,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+    draw(window, background, bg_image)
     pygame.quit()
     quit()
 
