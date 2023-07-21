ac5aeac10889a47e107a4908324157815202f390
Baizak04 <nadurbekovbaizak@gmail.com>
2023-07-21 15:40:23 +0600

    change dict for

--- python3/python3/pythonist/dict_for.py
+++ python3/python3/pythonist/dict_for.py
@@ -1,7 +1,18 @@
-# dict_one = {
-#     'id': 1,
-#     'name': 'Ashutosh',
-# }
-# dict_two = {
-#     'books':
-# }
+dict_one = {
+    'id': 1,
+    'name': 'Ashutosh',
+}
+dict_two = {
+    'books': ['python', 'DSA'],
+    'coollege': 'NSFC',
+}
+dict_three = {
+    'city': 'Kolkata',
+    'country': 'India'
+}
+
+dict_one.update(dict_three)
+print(dict_one)
+
+merged_dict = dict_one | dict_two | dict_three
+print(merged_dict)
