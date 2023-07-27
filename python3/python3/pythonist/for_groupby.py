1dcfed59182014b4eec5ba27bf9a0045ebc488ca
Baizak04 <nadurbekovbaizak@gmail.com>
2023-07-27 22:38:20 +0600

    add code for chain()

--- python3/python3/pythonist/for_groupby.py
+++ python3/python3/pythonist/for_groupby.py
@@ -1,5 +1,15 @@
-from itertools import groupby
+from itertools import groupby, chain
 
 
+# 1)
 for key, group in groupby('prrrrrooooooggggrrraaamingggg'):
     print(key, list(group))
+    
+# 2)
+list_a = [1, 23]
+list_b = [3, 23]
+list_c = [60, 50]
+
+for i in chain(list_a, list_b, list_c):
+    print(i)
+# chain() помогает нам объединять несколько итераторов в один
