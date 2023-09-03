4a617330cb32e6d0e05761f4bfa9edd5b3d50c55
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-03 12:55:10 +0600

    add code functionc input()

---
+++ easy way to learn Python book/input_1.py
@@ -0,0 +1,30 @@
+sample = input("Which country do you belong to?: ")
+print(sample + " is a great country")
+
+a = input()
+print("hello " + a)
+
+sum1 = 467
+sum2 = 233
+print(sum1 + sum2)
+
+num1 = 323
+num2 = 122
+print(num1 - num2)
+
+number1 = 9
+number2 = 11
+print(number1 * number2)
+
+sum1 = 67
+sum2 = 4
+print(sum1 / sum2)
+
+numpy1 = 99
+numpy2 = 11
+print(numpy1 % numpy2)
+
+x = 106
+y = 11
+print(x // y)
+
