fd8c7299e2b9e23cc2f06fb393a7783d8e40d6c1
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-04 13:35:25 +0600

    add code if2

---
+++ easy way to learn Python book/if2.py
@@ -0,0 +1,19 @@
+number = 43
+if number % 3 == 0:
+    print ("This number is divided by 3")
+else:
+    print ("This number is not divisible by 3")
+    
+number = 40
+if number % 4 == 0:
+    print ("This number is divided by 4")
+else:
+    print ("This number is not divisible by 4")
+    
+sample = 45
+if sample % 3 == 0:
+    print ("This number is divisible by 3")
+elif sample % 4 == 0:
+    print("This number is divisible by 4")
+else:
+    print ("This number is not divisible by 3 and 4")
