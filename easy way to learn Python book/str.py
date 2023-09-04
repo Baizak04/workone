b746aa1fb52d704409163b9a2c3768406a329b23
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-04 13:16:29 +0600

    add code str

---
+++ easy way to learn Python book/str.py
@@ -0,0 +1,22 @@
+example = 'Today it will rain'
+print(len(example))
+
+example = 'Great' * 3
+print(example)
+
+example = "This is great"
+sample = example.find('gr')
+print(sample)
+
+example = "China is the most populous country"
+sample = example.title()
+print(sample)
+
+example = ['Ohio', 'Nevada', 'Colorado']
+print(example[0])
+
+x = [[1, 223], [245, 63, 22]]
+print(x[1][2])
+
+sample = [12, 32, 21, 24, 65]
+print(sample[0:2])
