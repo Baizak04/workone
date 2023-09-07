bbd5749dded2fe11d4f146893b8ec64c54790d7c
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-07 20:15:55 +0600

    add code number1

---
+++ easy way to learn Python book/number1.py
@@ -0,0 +1,38 @@
+# var_one = .5
+# var_two = 5 // var_one
+# print(var_two)
+
+# vat = 0,2 * 5
+# print(vat)
+
+# print(int(21) + float(21) * 0)
+
+# print("{0:*^9}".format(1234567))
+
+# var_one = 2, 5
+# var_two = 4
+# print(var_one * var_two) 
+
+# print(True + 100)
+
+var1, _, var2 = 3, 2, 1
+print((var1 + var2) / _)
+
+print(bool(2022 % 2 + (15 - 4 ** 2)))
+
+res = ord('c') - ord('a')
+print(res)
+
+i = 0xFF
+print(type(i))
+
+x = 4
+print(type(type(x)))
+
+x = 4
+print(type(range(x)))
+
+print(round(12.5) - round(11.5))
+
+print(round(2.5))
+print(round(3.5))
