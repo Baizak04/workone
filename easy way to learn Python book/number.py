8eae0228a44feff552a1c9155b119cd5a99dd51b
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-07 20:17:11 +0600

    add cdoe number

--- easy way to learn Python book/number.py
+++ easy way to learn Python book/number.py
@@ -6,3 +6,10 @@ print(f"Это рост составляет {my_height}см")
 f = "{}, {}"
 print(f.format(True, False))
 print("ad\v da")
+
+square = pow(5, 4)
+print(square)
+
+example_string = "I want to be on Dev's Battle team"
+new_string = example_string.lower()
+print(new_string)
