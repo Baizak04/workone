3b790ad0c2d1932cdace5c2a3caf595717dfc562
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-21 12:40:09 +0600

    add code tuple

---
+++ Dev's Battle task/tuple_one.py
@@ -0,0 +1,16 @@
+colors = ('red', 'green', 'black')
+print(type(colors))
+
+colors = ('red', 'green', 'black')
+color = colors.index('green')
+print(color)
+
+def daylight():
+    sunrise = '4:30'
+    sunset = '22:30'
+    
+    return sunrise, sunset
+
+start, finish = daylight()
+print(start)
+print(finish)
