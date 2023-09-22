65e86c52b3b44b521ebf2d6747c0814c73f861c9
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-23 02:17:56 +0600

    add code if3.py

---
+++ python3/Dev's Battle Task/if3.py
@@ -0,0 +1,35 @@
+# tweet_limit = 280
+# tweet_string = "Blah" * 50
+# diff = tweet_limit - len(tweet_string)
+# if diff >= 0:
+#     print("Соответствующий твит")
+# else:
+#     print("Прошел мимо", abs(diff))
+    
+# tasks = 'взять перчатки, взять маску, дать кошке витамины, вызвать скорую помощь'
+# tasks1 = tasks.split(',')
+# print(tasks1)
+
+# setup = "утка заходит в бар..."
+# setup1 = setup.replace('утка', 'мартышка')
+# print(setup1)
+
+
+# setup = 'утка заходит в бар...'
+# setup1 = setup.strip('.')
+# print(setup1)
+
+# sum1 = [1, [2, 3]]
+# sum2 = sum1
+# sum3 = sum1[:]
+
+# sum1[1][0] = 1
+# print(sum2, sum3)
+
+# x = 10
+# if x > 5 and x < 15:
+#     print("Yes")
+    
+y = 18
+if 7 < y < 28:
+    print("Да")
