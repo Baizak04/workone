faca90d38580b3d45ba5e9823b26ce7cb28c370f
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-28 18:32:26 +0600

    add code while.py

---
+++ Dev's Battle task/while.py
@@ -0,0 +1,52 @@
+# target = [1, 2, 3, 4, 5, 6, 7]
+
+# ind = 0
+
+# while ind < len(target):
+#     print(ind)
+#     ind += 1
+    
+
+# target = [1, 2, 3, 4, 5, 6, 7]
+
+# t = []
+
+# while len(target):
+#     el = target.pop(0)
+#     if el % 2:
+#         continue
+#     t.append(el ** 2)
+    
+# print(t)
+
+# x = 1
+
+# while x <= 10:
+#     print(x)
+#     x += 1
+# else:
+#     print('Done')
+    
+# x = 1
+
+# while x <= 10:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+    
+# a = 0
+# n = 10
+# while a != n:
+#     b = a * a
+#     a += 2
+#     print(b)
+    
+a = 1
+
+while a < 3:
+    print('Условие верно')
+    a += 1
+else:
+    print('Условие неверно')
+    
