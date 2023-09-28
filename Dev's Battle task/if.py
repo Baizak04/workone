6f12a5148df9b218f99726966c08c4ea6895816e
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-28 14:33:16 +0600

    add code if

--- Dev's Battle task/if.py
+++ Dev's Battle task/if.py
@@ -1,21 +1,60 @@
-word1 = 'python'
-word2 = 'colab'
-
-counter = 0
-
-if 'p' in word1.lower():
-    counter += 1
-
-if 'p' in word2.lower():
-    counter += 1
+f = None
+if f is not None and len(f) > 0:
+    print('1')
+else:
+    print('0')
     
-print(counter)
+password = 'testqwerty12345'
+
+if password.isalnum():
+    print(1)
+else:
+    print(2)
 
 
-my_list = [2, 1, 3]
+code_language = 'Python'
+print(f'{code_language} is first language')
 
-for element in my_list:
-    if element == 1:
-        print('Это единичка')
+print(sorted)
+
+if __debug__:
+    print('yes')
+else:
+    print('no')
+    
+*c, last = [1, 2, 3]
+print(*c)
+
+x = True
+c = 0
+while x:
+    c += 1
+    if c == 10:
+        x = False
+    print(c)
+    
+    
+temp = 10
+
+if temp > 10:
+    print('Холодно')
+elif 10 <= temp < 28:
+    if 10 <= temp < 15:
+        print('Холодно но нормально')  
     else:
-        print('Это не единичка')
+        print('Очень хорошо, но нормально')
+else:
+    print('Очень жарко')
+        
+        
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print('Вы высокий мужчина')
+elif is_male and not(is_tall):
+    print('Вы невысокий мужчина')
+elif not(is_male) and is_tall:
+    print('Вы не являетесь мужчиной, но имеете высокий рост')
+else:
+    print('Вы либо не мужчина, либо не высокого роста, либо и то и другое вместе')
