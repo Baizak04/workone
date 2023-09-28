f3932a7d95171061002e8555fca4c2bef24893ec
Baizak04 <nadurbekovbaizak@gmail.com>
2023-09-28 18:38:48 +0600

    add code try except

--- Dev's Battle task/try_except.py
+++ Dev's Battle task/try_except.py
@@ -4,3 +4,10 @@ except IndexError:
     print('Получено искличение')
 else:
     print('Но в этом нет ничего страшного')
+    
+books = ['Cracking the Coding Interview', 'Clean code', 'The Pragmatic Programmer']
+try:
+    ind = books.index('Design Patterns')
+except ValueError:
+    ind = -1
+print(ind)
