3a4a9622b7e62972cede3c459088ef3721f25203
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-10 17:02:39 +0600

    variable task

---
+++ Dev's Battle task2/variable.py
@@ -0,0 +1,23 @@
+a, b, c = 11, 22, 33
+print(c, b, a)
+
+a, b = "Hello", "Python"
+print(len(a) + len(b))
+
+
+a = "Dev'"
+b = "Battle"
+a = a + 's' ' ' + b
+print(a)
+
+# a = 9.1
+# b = "Python"
+# print(len(a) + len(b))
+
+# a = 9.1
+# b = "Python"
+# print(min(a) + max(b))
+
+
+n = "@I_am_your_kitten"
+print("Я продаю подшипник. Ссылка: {} Лицензионный.".format(n))
