3729acdd93ed64a87f268b40ca12f9ff9d1781aa
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-12 18:44:40 +0600

    add code list task

---
+++ Dev's Battle task2/list1.py
@@ -0,0 +1,44 @@
+# 1)
+a = [1, 2, 3, 4]
+b = a.index(2)
+c = a[1]
+print(c + b)
+
+# 2)
+def f1():
+    foo = [11, 22, 33]
+    out = ''
+    for  i in range(len(foo)):
+        out += str(i) + '->' + str(foo[i])
+    print(out)
+    
+    
+f1()
+
+# 3)
+def f2():
+    foo = [11, 22, 77]
+    s = 0
+    i = 0
+    while i < len(foo):
+        s += foo[i]
+        i += 1
+    print(s)
+    
+    
+f2()
+
+
+def f3():
+    foo = [1, 32, 33, 2, 31]
+    max = foo[0]
+    
+    for item in foo:
+        if (item > max):
+            max = item
+            
+    print(max)
+    
+
+f3()
+
