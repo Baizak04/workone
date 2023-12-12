45d653f3017a3957a82d741260768314e4251cf5
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-12 17:19:44 +0600

    continue practic while task

--- Dev's Battle task2/while.py
+++ Dev's Battle task2/while.py
@@ -1,4 +1,35 @@
 i = 5
-while i < 8:
-    print(i)
-    i = i + 1
+# while i < 8:
+#     print(i)
+#     i = i + 1
+    
+# # 2)
+# i = 5
+# while i < 8:
+#     i = i + 1
+#     print(i)
+    
+# # 3) 
+def f1(n):
+    out = ''
+    i = 0
+    while i <= n:
+        out += str(i) + '_'
+        i += 3
+    print(out)
+    
+    
+f1(7)
+
+
+# 4)
+def f1(n):
+    out = ''
+    i = 0
+    while i <= n:
+        out += ' :)'
+        i += 3
+    print(out)
+    
+    
+f1(7)
