e6da11cb584121ff75fadfa8265ecb48432c83b1
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-15 18:39:51 +0600

    math module two

---
+++ Dev's Battle task/basic_python_task/math_two.py
@@ -0,0 +1,21 @@
+from math import *
+
+
+print(-round(52 / 26 + pow(5, 2)))
+
+from math import *
+
+
+print(sqrt(100))
+
+# 4)
+from math import *
+
+
+print(ceil(23.0002))
+
+# 5)
+from math import *
+
+ 
+print(factorial(5))
