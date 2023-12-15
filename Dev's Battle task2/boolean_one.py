98f6ea4aff7c2483615bef5590034e8410821f92
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-15 16:41:37 +0600

    add code bool

---
+++ Dev's Battle task2/boolean_one.py
@@ -0,0 +1,24 @@
+# # 1) 
+a = True
+b = False
+print('a and b', a and b)
+
+
+# # 2) 
+a = True
+b = False
+print('a or b', a or b)
+
+
+# # 3)
+a = True
+b = False
+print('a not b', not a, not b)
+
+
+# # 4)
+bool = False
+if bool:
+    print('Python')
+else:
+    pass
