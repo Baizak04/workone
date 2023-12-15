232bb61bbd92c6dc32be9135ea4f2488e3f3d07d
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-15 17:23:02 +0600

    add code numbers

---
+++ Dev's Battle task2/numbers_one.py
@@ -0,0 +1,29 @@
+# 1)
+n = 8834
+n = n // pow(10, 3) * pow(10, 3)
+print(n)
+
+# 2)
+print(r"\n")
+# 3)
+print("s = \"\"")
+
+# 4)
+def true():
+    print("inside true")
+    return True
+
+def false():
+    print("inside false")
+    return False
+
+
+a = true() or false()
+print(a)
+
+
+# 5)
+gpa = 4
+print(abs(gpa * -3))
+
+
