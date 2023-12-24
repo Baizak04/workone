040def7704fc8bc3a1fc4160ffcc09a0292c6a3c
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-24 16:29:06 +0600

    add code task 5 book

---
+++ the_self-taught_programmer_book/chapter2/task_book.py
@@ -0,0 +1,27 @@
+# # 1)
+# print("as")
+# print("ae")
+# print("aw")
+
+# 2)
+a = 10
+if a > 10:
+    print("a")
+elif a <= 10:
+    print("b")
+    
+# 3)
+print(20 % 2)
+
+# 4) 
+print(21 // 3)
+
+
+# 5) 
+age = 64
+retirement = age - 65
+
+if retirement < 10:
+    print("aa")
+else:
+    print("bb")
