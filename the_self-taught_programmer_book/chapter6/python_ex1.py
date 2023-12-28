5d88dddf060b39cb0065025a93f6175a236663c9
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-28 13:07:45 +0600

    python example1

---
+++ the_self-taught_programmer_book/chapter6/python_ex1.py
@@ -0,0 +1,23 @@
+author = "Baizak"
+print(author[3])
+
+
+
+author = "Baizak"
+print(author[2])
+
+number_and_world = "Great"
+print(number_and_world * 3)
+
+
+world_one = 'SAS'
+print(world_one.lower())
+
+world_one = 'SAS'
+print(world_one.capitalize())
+
+world_one = 'Sas'
+print(world_one.upper())
+
+
+
