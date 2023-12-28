7b0363c1be31cae2a55070008a01a9425bc3c789
Baizak04 <nadurbekovbaizak@gmail.com>
2023-12-28 13:08:39 +0600

    add code python example chapter7 book

---
+++ the_self-taught_programmer_book/chapter7/python_ex1.py
@@ -0,0 +1,30 @@
+# 1)
+# str
+worlds = "Python 3.12"
+for world in worlds:
+    print(world)
+    
+# 2) 
+# list
+words_list = ['Go', 'Java', 'JavaScript']
+for world_list in words_list:
+    print(world_list)
+    
+# 3)
+# tuple
+words_tuple = ('C', 'C#', 'C+', 'C++')
+for world_tuple in words_tuple:
+    print(world_tuple)
+    
+# 4)
+# dict
+words_dict = {'Go': 2,
+              'Python': 3.12,
+              'JavaScript': 2.23
+              }
+
+for key in words_dict:
+    print(key)
+    
+
+
