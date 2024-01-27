874f482e150985666ba67e7b02daaa034beeec43
Baizak04 <nadurbekovbaizak@gmail.com>
2024-01-27 13:24:58 +0600

    basic read book

---
+++ book/express_python/express1.py
@@ -0,0 +1,22 @@
+number: int = 1 * 2 - 4
+print(number)
+
+number_complex: complex = (2 + 4j) * (2 + 3j)
+print(number_complex)
+
+bool_one: bool = True
+print(not bool_one)
+
+
+list_number: list = [1, 2, 3, 4]
+print(list_number)
+
+list_world: list = ["go", "i", "you", "pg"]
+print(list_world[1])
+
+
+list_number_one: list = [1, 3, 4, 5, 7]
+list_number_one: list = list_number_one * 2 + [1, 3]
+print(list_number_one) 
+
+print(round(1/3, 2))
